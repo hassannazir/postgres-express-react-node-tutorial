0267e917f845db4adf5a51958f8c1a2c3ff568e6
hassannazir <hassan.nazir1998@gmail.com>
2021-05-06 18:28:11 +0500

    first commit

---
+++ server/controllers/todos.js
@@ -0,0 +1,96 @@
+const Todo = require('../models').Todo;
+const TodoItem=require('../models').TodoItem;
+
+module.exports = {
+  create(req, res) {
+    return Todo
+      .create({
+        title: req.body.title,
+      })
+      .then(todo => res.status(201).send(todo))
+      .catch(error => res.status(400).send(error));
+  },
+
+  //Return TODOS only
+  // list(req, res) {
+  //   return Todo
+  //     .findAll()
+  //     .then(todos => res.status(200).send(todos))
+  //     .catch(error => res.status(400).send(error));
+  // },
+
+  //return todos and todoitems inside todos
+  list(req, res) {
+    return Todo
+      .findAll({
+        include: [{
+          model: TodoItem,
+          as: 'todoItems',
+        }],
+      })
+      .then(todos => res.status(200).send(todos))
+      .catch(error => res.status(400).send(error));
+  },
+
+  retrieve(req, res) {
+    return Todo
+      .findByPk(req.params.todoId, {
+        include: [{
+          model: TodoItem,
+          as: 'todoItems',
+        }],
+      })
+      .then(todo => {
+        if (!todo) {
+          return res.status(404).send({
+            message: 'Todo Not Found',
+          });
+        }
+        return res.status(200).send(todo);
+      })
+      .catch(error => res.status(400).send(error));
+  },
+
+  update(req, res) {
+    return Todo
+      .findByPk(req.params.todoId, {
+        include: [{
+          model: TodoItem,
+          as: 'todoItems',
+        }],
+      })
+      .then(todo => {
+        if (!todo) {
+          return res.status(404).send({
+            message: 'Todo Not Found',
+          });
+        }
+        return todo
+          .update({
+            title: req.body.title || todo.title,
+          })
+          .then(() => res.status(200).send(todo))  // Send back the updated todo.
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+
+  destroy(req, res) {
+    return Todo
+      .findByPk(req.params.todoId)
+      .then(todo => {
+        if (!todo) {
+          return res.status(400).send({
+            message: 'Todo Not Found',
+          });
+        }
+        return todo
+          .destroy()
+          .then(() =>res.status(200).send({
+            message: 'Todo Deleted Successfully',
+          }))
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
+  },
+};
